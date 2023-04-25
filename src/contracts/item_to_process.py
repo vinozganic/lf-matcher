1944f7d53538be3495c14fdccd251b726dbe510d
Marko <marko.bolt4@gmail.com>
2023-04-25 17:17:49 +0200

    removed identifiable property

--- src/contracts/item_to_process.py
+++ src/contracts/item_to_process.py
@@ -1,5 +1,5 @@
 class item_to_process:
-    def __init__(self, item_type, id, type, subtype, color, location, date, identifiable):
+    def __init__(self, item_type, id, type, subtype, color, location, date):
         self.item_type = item_type
         self.id = id
         self.type = type
@@ -7,7 +7,6 @@ class item_to_process:
         self.color = color
         self.location = location
         self.date = date
-        self.identifiable = identifiable
     
     @classmethod
     def from_dict(cls, item_dict: dict):
@@ -15,5 +14,5 @@ class item_to_process:
             item_type=item_dict["item_type"], id=item_dict["id"],
             color=item_dict["color"], location=item_dict["location"],
             date=item_dict["date"], type=item_dict["type"],
-            subtype=item_dict["subtype"], identifiable=item_dict["identifiable"]
+            subtype=item_dict["subtype"]
         )
