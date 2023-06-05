edc9714b44f59e5f7fd724f771d564224077b8d3
Marko <marko.bolt4@gmail.com>
2023-06-05 23:10:35 +0200

    feature engineering and model improvement

--- src/data_transformer.py
+++ src/data_transformer.py
@@ -10,7 +10,7 @@ from colormath.color_objects import sRGBColor, LabColor
 from colormath.color_conversions import convert_color
 from gensim.models import KeyedVectors
 from pyproj import CRS, Transformer
-from shapely.geometry import Point, MultiLineString
+from shapely.geometry import Point, MultiLineString, LineString
 from shapely.ops import unary_union, transform
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -31,27 +31,39 @@ class DataTransformer:
     def prepare_data(self, lost_item : item_to_process, found_item: item_to_process):
         type_similarity = self._compute_type_similarity(lost_item.type, found_item.type)
         color_distance = self._compute_color_distance(lost_item.color, found_item.color)
-        min_distance, centroid_distance, overlap_area, overlap_ratio, = self._compute_location_parameters(lost_item.location, found_item.location)
+        same_transport_line_usage, lost_path_presence, found_path_presence, lost_public_transport_lines_presence, found_public_transport_lines_presence, path_overlap_ratio, public_transport_lines_overlap_ratio, weighted_path_overlap_ratio, weighted_public_transport_lines_overlap_ratio, min_distance, centroid_distance, overlap_ratio, = self._compute_location_parameters(lost_item.location, found_item.location)
         date_distance = self._compute_date_distance(lost_item.date, found_item.date)
 
         return {
             "type_similarity": type_similarity,
             "color_distance": color_distance,
+            "same_transport_line_usage": same_transport_line_usage,
+            "lost_path_presence": lost_path_presence,
+            "found_path_presence": found_path_presence,
+            "lost_public_transport_lines_presence": lost_public_transport_lines_presence,
+            "found_public_transport_lines_presence": found_public_transport_lines_presence,
+            "path_overlap_ratio": path_overlap_ratio,
+            "public_transport_lines_overlap_ratio": public_transport_lines_overlap_ratio,
+            # "weighted_path_overlap_ratio": weighted_path_overlap_ratio,
+            # "weighted_public_transport_lines_overlap_ratio": weighted_public_transport_lines_overlap_ratio,
             "location_min_distance": min_distance,
             "location_centroid_distance": centroid_distance,
             # "location_overlap_area": overlap_area,
-            "location_overlap_ratio": overlap_ratio,
+            # "location_overlap_ratio": overlap_ratio,
             "date_distance": date_distance,
         }
 
     def _compute_type_similarity(self, lost_item_type, found_item_type):
         if self.type_similarity_matrix.get(lost_item_type) is None:
             return 0
-        return self.type_similarity_matrix[lost_item_type].get(found_item_type, 0)
-    
+        similarity = self.type_similarity_matrix[lost_item_type].get(found_item_type, 0)
+        if not similarity:
+            similarity = self.type_similarity_matrix[found_item_type].get(lost_item_type, 0)
+        return similarity
+
     def _load_type_similarity_matrix(self):
         current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        matrix = json.load(open(current_dir + "/model/type_similarity_matrix.json"))
+        matrix = json.load(open(current_dir + "/model/type_similarity_matrix_gpt4.json"))
         return matrix
 
     def _compute_color_distance(self, lost_item_color, found_item_color):
@@ -65,15 +77,26 @@ class DataTransformer:
         return delta_e
     
     def _compute_date_distance(self, lost_item_date, found_item_date):
-        # lost_date = datetime.strptime(lost_item_date, "%Y-%m-%dT%H:%M:%S.%fZ")
-        # found_date = datetime.strptime(found_item_date, "%Y-%m-%dT%H:%M:%S.%fZ")
-        return abs((lost_item_date - found_item_date).days)
+        return (found_item_date - lost_item_date).days
 
     def _compute_location_parameters(self, lost_item_location, found_item_location):
-        lost_geom = self._get_geometry(lost_item_location)
-        found_geom = self._get_geometry(found_item_location)
+        same_transport_line_usage = self._get_same_transport_line_usage(lost_item_location, found_item_location)
+
+        path_overlap_ratio = self._get_path_overlap_ratio(lost_item_location, found_item_location)
+        public_transport_lines_overlap_ratio = self._get_public_transport_lines_overlap_ratio(lost_item_location, found_item_location)
+
+        lost_path_presence = self._get_path_presence(lost_item_location)
+        found_path_presence = self._get_path_presence(found_item_location)
         
+        lost_public_transport_lines_presence = self._get_public_transport_lines_presence(lost_item_location)
+        found_public_transport_lines_presence = self._get_public_transport_lines_presence(found_item_location)
+
+        weighted_path_overlap_ratio = self._get_weighted_path_overlap_ratio(lost_path_presence, found_path_presence, lost_public_transport_lines_presence, found_public_transport_lines_presence, path_overlap_ratio)
+        weighted_public_transport_lines_overlap_ratio = self._get_weighted_public_transport_lines_overlap_ratio(lost_path_presence, found_path_presence, lost_public_transport_lines_presence, found_public_transport_lines_presence, public_transport_lines_overlap_ratio)
+
+        lost_geom = self._get_geometry(lost_item_location)
         lost_geom = self._transform_geometry_to_wgs84(lost_geom)
+        found_geom = self._get_geometry(found_item_location)
         found_geom = self._transform_geometry_to_wgs84(found_geom)
 
         lost_geom_buffer = lost_geom.buffer(500)
@@ -84,7 +107,30 @@ class DataTransformer:
         overlap_area = self._get_overlap_area(lost_geom_buffer, found_geom_buffer)
         overlap_ratio = self._get_overlap_ratio(lost_geom_buffer, found_geom_buffer, overlap_area)
 
-        return min_distance, centroid_distance, overlap_area, overlap_ratio
+        return same_transport_line_usage, lost_path_presence, found_path_presence, lost_public_transport_lines_presence, found_public_transport_lines_presence, path_overlap_ratio, public_transport_lines_overlap_ratio, weighted_path_overlap_ratio, weighted_public_transport_lines_overlap_ratio, min_distance, centroid_distance, overlap_ratio
+    
+    def _get_path_presence(self, item_location):
+        if item_location.get("path"):
+            return 1
+        return 0
+    
+    def _get_public_transport_lines_presence(self, item_location):
+        if item_location.get("publicTransportLines"):
+            return 1
+        return 0
+
+    def _get_same_transport_line_usage(self, lost_item_location, found_item_location):
+        lost_transport_lines = lost_item_location.get("publicTransportLines")
+        found_transport_lines = found_item_location.get("publicTransportLines")
+        if not lost_transport_lines or not found_transport_lines:
+            return 0
+        lost_transport_lines = [line["coordinates"] for line in lost_transport_lines]
+        found_transport_lines = [line["coordinates"] for line in found_transport_lines]
+        for lost_line in lost_transport_lines:
+            for found_line in found_transport_lines:
+                if lost_line == found_line:
+                    return 1
+        return 0
 
     def _get_geometry(self, item_location):
         path_geom = None
@@ -135,6 +181,85 @@ class DataTransformer:
     def _get_overlap_ratio(self, lost_geom_buffer, found_geom_buffer, overlap_area):
         return overlap_area / (lost_geom_buffer.area + found_geom_buffer.area - overlap_area)
     
+    def _get_path_overlap_ratio(self, lost_item_location, found_item_location):
+        # the possible geometry types are Point and MultiLineString
+        # create buffer around the them and calculate the overlap ratio
+        lost_path = lost_item_location.get("path")
+        found_path = found_item_location.get("path")
+        if lost_path is None or found_path is None:
+            return 0
+        lost_path_geom = None
+        found_path_geom = None
+        if lost_path["type"] == "Point":
+            lost_path_geom = Point(lost_path["coordinates"])
+        elif lost_path["type"] == "MultiLineString":
+            lost_path_geom = MultiLineString(lost_path["coordinates"])
+        if found_path["type"] == "Point":
+            found_path_geom = Point(found_path["coordinates"])
+        elif found_path["type"] == "MultiLineString":
+            found_path_geom = MultiLineString(found_path["coordinates"])
+        if lost_path_geom is None or found_path_geom is None:
+            return 0
+        
+        lost_path_geom = self._transform_geometry_to_wgs84(lost_path_geom)
+        found_path_geom = self._transform_geometry_to_wgs84(found_path_geom)
+
+        lost_path_geom_buffer = lost_path_geom.buffer(500)
+        found_path_geom_buffer = found_path_geom.buffer(500)
+        overlap_area = lost_path_geom_buffer.intersection(found_path_geom_buffer).area
+        return overlap_area / (lost_path_geom_buffer.area + found_path_geom_buffer.area - overlap_area)
+
+    from shapely.ops import unary_union
+
+    def _get_public_transport_lines_overlap_ratio(self, lost_item_location, found_item_location):
+        """
+        Calculate the overlap ratio between public transport lines of a lost and found item.
+        
+        Parameters:
+        - lost_item_location: Location information of the lost item
+        - found_item_location: Location information of the found item
+        
+        Returns:
+        - The overlap ratio between the public transport lines of the lost and found items
+        """
+        lost_public_transport_lines = lost_item_location.get("publicTransportLines")
+        found_public_transport_lines = found_item_location.get("publicTransportLines")
+        if not lost_public_transport_lines or not found_public_transport_lines:
+            return 0
+
+        lost_public_transport_lines_geom = [
+            self._transform_geometry_to_wgs84(LineString(line["coordinates"]))
+            for line in lost_public_transport_lines
+        ]
+        found_public_transport_lines_geom = self._transform_geometry_to_wgs84(
+            LineString(found_public_transport_lines[0]["coordinates"])
+        )
+
+        lost_public_transport_lines_geom_buffer = [
+            line.buffer(500)
+            for line in lost_public_transport_lines_geom
+        ]
+        found_public_transport_lines_geom_buffer = found_public_transport_lines_geom.buffer(500)
+
+        lost_public_transport_lines_geom_buffer_union = unary_union(lost_public_transport_lines_geom_buffer)
+
+        overlap_area = lost_public_transport_lines_geom_buffer_union.intersection(
+            found_public_transport_lines_geom_buffer).area
+
+        total_area = lost_public_transport_lines_geom_buffer_union.area + found_public_transport_lines_geom_buffer.area - overlap_area
+
+        return overlap_area / total_area
+    
+    def _get_weighted_path_overlap_ratio(self, lost_path_presence, found_path_presence, lost_public_transport_lines_presence, found_public_transport_lines_presence, path_overlap_ratio):
+        if lost_path_presence == 1 and found_path_presence == 1:
+            return path_overlap_ratio
+        return 0
+    
+    def _get_weighted_public_transport_lines_overlap_ratio(self, lost_path_presence, found_path_presence, lost_public_transport_lines_presence, found_public_transport_lines_presence, public_transport_lines_overlap_ratio):
+        if lost_public_transport_lines_presence == 1 and found_public_transport_lines_presence == 1:
+            return public_transport_lines_overlap_ratio
+        return 0
+
     def get_types_from_db(self):
         response = requests.get(f"{API_URL}/config/types").json()
         if response["success"]:
@@ -148,6 +273,7 @@ class DataTransformer:
             return self.type_similarity_matrix
 
         all_types = self.get_types_from_db()
+        print("All types: ", all_types)
 
         current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         model = KeyedVectors.load_word2vec_format(current_dir + "/model/glove.42B.300d.txt", binary=False, no_header=True)
--- src/model.py
+++ src/model.py
@@ -2,8 +2,9 @@ import os
 import pickle
 
 import pandas as pd
+from sklearn.calibration import CalibratedClassifierCV
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score, f1_score
 
 
@@ -18,7 +19,7 @@ class Model:
         print(f"Prediction: {prediction}")
         return prediction[0][1]
 
-    def train(self):
+    def train(self, grid_search=False):
         current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         df = pd.read_csv(current_dir + "/model/data.csv")
 
@@ -27,44 +28,69 @@ class Model:
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41)
 
-        models = {
-            # "logistic_regression": LogisticRegression(),
-            # "knn": KNeighborsClassifier(),
-            # "decision_tree": DecisionTreeClassifier(),
-            # "svm": SVC(probability=True),
-            "random_forest": RandomForestClassifier(n_estimators=1000),
-        }
+        params = None
+        if grid_search:
+            rf = RandomForestClassifier()
 
-        best_model = None
-        best_model_accuracy = 0
+            param_grid = {
+                'n_estimators': [100, 200, 300],  # The number of trees in the forest.
+                'criterion': ['gini', 'entropy'],  # The function to measure the quality of a split.
+                'max_depth': [None, 10, 20, 30],  # The maximum depth of the tree.
+                'min_samples_split': [2, 5, 10],  # The minimum number of samples required to split an internal node.
+                'min_samples_leaf': [1, 2, 4],  # The minimum number of samples required to be at a leaf node.
+                'max_features': ['auto', 'sqrt', 'log2', None],  # The number of features to consider when looking for the best split.
+                'bootstrap': [True, False],  # Whether bootstrap samples are used when building trees.
+                'class_weight': ['balanced', 'balanced_subsample', None]  # Weights associated with classes. This could be useful if you have imbalance between classes.
+            }
+
+            print("Choosing best hyperparameters...")
+            grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5, verbose=2, n_jobs=-1)
+            grid_search.fit(X_train, y_train)
+
+            best_params = grid_search.best_params_
+            params = best_params
+            print("Best hyperparameters: ", best_params)
+
+        if not params:
+             params = {
+                "bootstrap": True,
+                "class_weight": "balanced_subsample",
+                "criterion": "gini",
+                "max_depth": None,
+                "max_features": "sqrt",
+                "min_samples_leaf": 4,
+                "min_samples_split": 10,
+                "n_estimators": 200
+            }
+
+        model = RandomForestClassifier(**params )
+
+        print("Training model using Random Forest Classifier...")
+
+        model.fit(X_train, y_train)
+
+        calibrator = CalibratedClassifierCV(model, method="isotonic")
+        calibrator.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        y_prob = model.predict_proba(X_test)
+
+        accuracy = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+
+        print("Accuracy score: ", accuracy, "F1 score: ", f1)
         
-        for name, model in models.items():
-            print("Training model: ", name)
-            model.fit(X_train, y_train)
+        # --- Uncomment to see feature importances ---
+        feature_names = X_train.columns
+        feature_importances = model.feature_importances_
 
-            y_pred = model.predict(X_test)
-            y_prob = model.predict_proba(X_test)
+        print("Feature importances: ")
+        for feature_name, feature_importance in zip(feature_names, feature_importances):
+                print(f"Feature: {feature_name}, Importance: {feature_importance}")
 
-            accuracy = accuracy_score(y_test, y_pred)
-            f1 = f1_score(y_test, y_pred)
+        self.test_model(model)
 
-            print("Accuracy score: ", accuracy, "F1 score: ", f1)
-            
-            # --- Uncomment to see feature importances ---
-            feature_names = X_train.columns
-            feature_importances = model.feature_importances_
-
-            print("Feature importances: ")
-            for feature_name, feature_importance in zip(feature_names, feature_importances):
-                 print(f"Feature: {feature_name}, Importance: {feature_importance}")
-
-            self.test_model(model)
-
-            if accuracy > best_model_accuracy:
-                best_model = model
-                best_model_accuracy = accuracy
-
-        self.model = best_model
+        self.model = model
         self.model.feature_names = X.columns
 
         pickle.dump(self.model, open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/model/model.pkl", "wb"))
